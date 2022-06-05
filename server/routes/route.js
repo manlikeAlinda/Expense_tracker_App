9b856b219aee3531f26b66e4dc9c7b960402233d
Alinda Alvin <ausvin60@gmail.com>
2022-06-05 05:02:27 -0700

    Yeah

--- server/routes/route.js
+++ server/routes/route.js
@@ -10,9 +10,9 @@ routes
   .route("/api/transaction")
   .post(controller.create_Transaction)
   .get(controller.get_Transaction)
-  .delete(controller.delete_Transaction);
+  .delete(controller.delete_Transaction)
 
 
-routes.route("/api/labels").get(controller.get_Labels);
+routes.route("/api/labels").get(controller.get_Labels)
 
 module.exports = routes;
